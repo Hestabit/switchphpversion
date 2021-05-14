95ed60fde2fbba931626019d1adf5ebcdffda990
Saurabh Ranjan <26545067+insaurabh@users.noreply.github.com>
2021-05-14 10:26:23 +0530

    php7.4 version added
    php7.4 version added with basic required extensions.

--- phpversion.sh
+++ phpversion.sh
@@ -55,6 +55,16 @@ then
         sudo update-alternatives --set php /usr/bin/php7.3
 sudo service apache2 restart
 
+elif [ "$phpenable" = "7.4" ]
+then 
+    sudo apt-get install libapache2-mod-php7.4
+    sudo apt install -y php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl \ 
+    php7.4-gd  php7.4-mysql php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring \
+     php7.4-opcache php7.4-soap php7.4-zip php7.4-intl 
+    sudo a2dismod php$phpdisable
+    sudo a2enmod  php7.4
+        sudo update-alternatives --set php /usr/bin/php7.4
+sudo service apache2 restart
 else
   echo "There is something wrong"
 fi
